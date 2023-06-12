f1bb4f6b868dc15e13e084b2454655570e2608f9
DenimB96 <rlsjr2@live.com>
2023-06-12 16:43:41 -0400

    setup models and associated models within index.js file

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// creating associations between models
+// import all models
+const Comment = require('./comment');
+const Post = require('./post');
+const User = require('./user');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
